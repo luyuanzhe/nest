5b4c07bcebf6a7ccf580b0bf393f0ec14dd6b460
luyuanzhe <1957997702@qq.com>
2026-06-09 02:51:35 +0000

    6-2

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ const projectDir = __dirname;
 const tsconfigPath = path.join(projectDir, 'tools/gulp/tsconfig.json');
 
 require('ts-node').register({
-  project: tsconfigPath
+  project: tsconfigPath,
+  transpileOnly: true
 });
 
 require('./tools/gulp/gulpfile');
