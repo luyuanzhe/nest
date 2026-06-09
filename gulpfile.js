7e77e7c5c82926346de93b2943a2779f5357484a
luyuanzhe <1957997702@qq.com>
2026-06-09 02:31:43 +0000

    6-1

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,16 @@ const path = require('path');
 
 const projectDir = __dirname;
 const tsconfigPath = path.join(projectDir, 'tools/gulp/tsconfig.json');
+const defaultConcurrency = process.env.LERNA_CONCURRENCY || '5';
+
+process.env.LERNA_CONCURRENCY = defaultConcurrency;
+process.env.NX_PARALLEL = process.env.NX_PARALLEL || defaultConcurrency;
+process.env.NX_CACHE_DIRECTORY =
+  process.env.NX_CACHE_DIRECTORY || path.join(projectDir, 'node_modules/.cache/nx');
 
 require('ts-node').register({
-  project: tsconfigPath
+  project: tsconfigPath,
+  transpileOnly: true
 });
 
 require('./tools/gulp/gulpfile');
