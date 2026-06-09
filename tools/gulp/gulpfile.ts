884185bda3973df300928cb159c44d3509ed40a6
luyuanzhe <1957997702@qq.com>
2026-06-09 02:59:22 +0000

    6-3

--- tools/gulp/gulpfile.ts
+++ tools/gulp/gulpfile.ts
@@ -1,4 +1,9 @@
+import { parallel, series, task } from 'gulp';
 import './tasks/clean';
 import './tasks/copy-misc';
 import './tasks/move';
 import './tasks/samples';
+
+task('move:all', parallel('move:node_modules', 'move:samples'));
+
+task('prerelease:gulp', series('clean:bundle', 'copy-misc'));
